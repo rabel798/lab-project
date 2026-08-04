a2846230642fd46888af5f92bf3bd9f0cb490ed1
Rahul <rabel798679@gmail.com>
2026-08-04 10:21:30 +0530

    trying waitress

--- app.py
+++ app.py
@@ -6,8 +6,12 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("main.html")
-
+    
+@app.route("/insert", methods=["POST"])
+def insert():
+    return render_template("insert.html")
 
 if __name__ == "__main__":
     from waitress import serve
+    print ("waitress serving")
     serve(app,host="0.0.0.0", port=5000)
